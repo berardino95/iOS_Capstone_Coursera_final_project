6c7a05a2299916823c18d64e139a5c9eff7f2e6f
Berardino Chiarello <berardino95@gmail.com>
2023-04-11 10:03:15 +0200

    Initial Commit

---
+++ iOS_Capstone_Coursera_final_project/iOS_Capstone_Coursera_final_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Capstone_Coursera_final_projectApp.swift
+//  iOS_Capstone_Coursera_final_project
+//
+//  Created by CHIARELLO Berardino - ADECCO on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Capstone_Coursera_final_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
